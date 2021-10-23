2d40040a334f9840da0bad20b22bb578fad3b1c1
shaharKamay <shahar.kamay@gmail.com>
2021-10-23 03:30:20 +0300

    added an option to delete and set data (catch and release)

--- src/index.js
+++ src/index.js
@@ -1,42 +1,93 @@
-const baseURL2 = "https://pokeapi.co/api/v2/";
+const baseUrlPokeApi = "https://pokeapi.co/api/v2/";
 const baseURL = 'http://localhost:8080/';
-let allPokemons= {};
+let allPokemons= {};// fixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-async function getData(url) {
+async function getData(url, username) {
     const response = await axios.get(url, {
         headers: {
-            username: 'max',
+            username,
         }
     });
     return response.data;
 }
 
+async function setData(url, username) {
+    await axios.put(url, {
+        headers: {
+            username,
+        }
+    })
+}
+
+async function deleteData(url, username) {
+    await axios.delete(url, {
+        headers: {
+            username,
+        }
+    })
+}
+
 async function getPokemonByName(name) {
-    const pokemon = await getData(`${baseURL}pokemon/get/${name}`);
+    const pokemon = await getData(`${baseURL}pokemon/query?name=${name}`, 'max');
     return pokemon;
 }
 
 async function getPokemonById(id) {
-    const pokemon = await getData(`${baseURL}pokemon/get/${id}`);
+    const pokemon = await getData(`${baseURL}pokemon/get/${id}`, 'max');
     return pokemon;
 }
 
-async function getAllPokemons() {
-    const pokemons = await getData(`${baseURL2}pokemon?limit=10000`);
+async function getAllPokemons() { //fixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    const pokemons = await getData(`${baseUrlPokeApi}pokemon?limit=10000`);
     return pokemons;
 }
 
 async function getPokemonImgByName(name) {
     const pokemon = await getPokemonByName(name);
-    return pokemon.sprites.front_default;
+    return pokemon.front_pic;
 }
 
 async function getPokemonsByType(type) {
-    const pokemons = await getData(`${baseURL2}type/${type}`);
+    const pokemons = await getData(`${baseUrlPokeApi}type/${type}`);
     return pokemons.pokemon;
 }
 
-function renderPokemonView(pokemon) {
+async function getUserPokemonsCollection(username) {
+    const pokemons = await getData(`${baseURL}pokemon/`, username);
+    return pokemons;
+}
+
+async function isPokemonExistsForUser(pokemonId, username) {
+    const userPokemons = await getUserPokemonsCollection(username);
+    for(const userPokemon of userPokemons) {
+        if(pokemonId === userPokemon.id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+async function catchPokemon(pokemonName, username) {
+    const pokemon = await getPokemonByName(pokemonName);
+    await setData(`${baseURL}pokemon/catch/${pokemon.id}`, username);
+}
+async function releasePokemon(pokemonName, username) {
+    const pokemon = await getPokemonByName(pokemonName);
+    await deleteData(`${baseURL}pokemon/release/${pokemon.id}`, username);
+}
+
+function renderCollectionView(pokemons) {
+    const pokemonCollection = document.getElementById("pokemon-collection");
+    removeChildren(pokemonCollection);
+    for(const pokemon of pokemons) {
+        const pokemonName = createElement('p', [pokemon.name], ['pokemon-collection__name']);
+        const pokemonImg = createElement('img', [], ['pokemon-collection__img'], { src: pokemon.front_pic });
+        const divButton = createElement('div', [pokemonName, pokemonImg], ['pokemon-collection__button']);
+        pokemonCollection.append(divButton);
+    }
+}
+
+async function renderPokemonView(pokemon) {
     const pokemonName = document.getElementById("pokemon-view__name");
     pokemonName.textContent = pokemon.name;
     const pokemonImg = document.getElementById("pokemon-view__img");
@@ -47,10 +98,13 @@ function renderPokemonView(pokemon) {
     pokemonWeight.textContent = "Weight: " + pokemon.weight;
     const pokemonTypes = document.getElementById("pokemon-view__types");
     removeChildren(pokemonTypes);
-    pokemonTypes.append("types: ");
+    pokemonTypes.append("Types: ");
     for(const type of pokemon.types) {
         pokemonTypes.append(createElement('button', [type.name], ["button"], {}, {click: clickTypeEventHandler} ));
     }
+    const pokemonAction = document.getElementById('pokemon-view__action');
+    removeChildren(pokemonAction);
+    pokemonAction.append(createElement('button', [await isPokemonExistsForUser(pokemon.id, 'max') ? 'Release' : 'Catch'], ['button'], {}, {click: clickCatchReleaseHandler}));
 
 }
 
@@ -70,9 +124,10 @@ function changePokemonAngleImg() {
 
 (async () => {
     allPokemons = await getAllPokemons();
-    const pokemon = await getPokemonById("1");
-    console.log(pokemon);
+    const pokemon = await getPokemonByName("bulbasaur");
     renderPokemonView(pokemon);
+    const pokemonsCollection = await getUserPokemonsCollection('max');
+    renderCollectionView(pokemonsCollection);
     // filterPokemons();
     document.querySelector(".pokemon-view__img").addEventListener("mouseover", changePokemonAngleImg);
     document.querySelector(".pokemon-view__img").addEventListener("mouseout", changePokemonAngleImg);
@@ -99,11 +154,25 @@ function searchByQuery(str, query) {
 
 async function clickPokemonPickEventHandler(e) {
     removeResultsSection();
-    const pokemonPickDiv = e.target.closest(".pokemon-pick__button");
+    const pokemonPickDiv = e.target.closest(".pokemon-collection__button");
     if(!pokemonPickDiv) return;
-    const pokemon = await getPokemonByName(pokemonPickDiv.querySelector(".pokemon-pick__name").textContent);
+    const pokemon = await getPokemonByName(pokemonPickDiv.querySelector(".pokemon-collection__name").textContent);
     renderPokemonView(pokemon);
 
+}
+
+async function clickCatchReleaseHandler(e) {
+    if(e.target.textContent === 'Catch') {
+        const pokemonName = document.getElementById('pokemon-view__name').textContent;
+        await catchPokemon(pokemonName, 'max');
+        renderCollectionView(await getUserPokemonsCollection('max'));
+        e.target.textContent = 'Release';
+    } else {
+        const pokemonName = document.getElementById('pokemon-view__name').textContent;
+        await releasePokemon(pokemonName, 'max');
+        renderCollectionView(await getUserPokemonsCollection('max'));
+        e.target.textContent = 'Catch';
+    }
 }
 
 async function searchEventHandler(e) {
@@ -134,14 +203,14 @@ async function renderResults(pokemons) {
             const img = createElement('img', [], ["result__img"], {src: await getPokemonImgByName(pokemon["pokemon"].name)});
             
             const imgDiv = createElement('div', [img], ["result__img-div"]);
-            const nameP = createElement('p', [pokemon["pokemon"].name], ["result__name", "pokemon-pick__name"]);
-            resultDiv = createElement('div', [imgDiv, nameP], ["search__result", "row", "pokemon-pick__button"]);
+            const nameP = createElement('p', [pokemon["pokemon"].name], ["result__name", "pokemon-collection__name"]);
+            resultDiv = createElement('div', [imgDiv, nameP], ["search__result", "row", "pokemon-collection__button"]);
         } else {
             const img = createElement('img', [], ["result__img"], {src: await getPokemonImgByName(pokemon.name)});
             
             const imgDiv = createElement('div', [img], ["result__img-div"]);
-            const nameP = createElement('p', [pokemon.name], ["result__name", "pokemon-pick__name"]);
-            resultDiv = createElement('div', [imgDiv, nameP], ["search__result", "row", "pokemon-pick__button"]);
+            const nameP = createElement('p', [pokemon.name], ["result__name", "pokemon-collection__name"]);
+            resultDiv = createElement('div', [imgDiv, nameP], ["search__result", "row", "pokemon-collection__button"]);
         }
         
         resultsSection.append(resultDiv);
